23e197ccc10a0110f8165a55afb9ff56bd107138
Teddy Otieno <teddy.asola@andela.com>
2017-09-27 20:17:36 +0300

    connect to db

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ if (env === 'development') {
 }
 const express = require('express'),
     morgan = require('morgan'),
+    bodyParser = require('body-parser'),
     path = require('path'),
     c = console,
+    sequelize = require('./server/config/db.js'),
     app = express();
 
+sequelize.authenticate().then(() => {
+    c.log("Connection has been established successfully!");
+}).catch((err) => {
+    c.log('Unable to connect to the database', err);
+});
 
 // log all requests to the console
 app.use(morgan('dev'));
+
+// parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // serve static files
 app.use(express.static('public'));
