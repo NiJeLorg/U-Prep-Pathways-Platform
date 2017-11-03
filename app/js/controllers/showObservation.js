dec2adf1e78ed266d9952952189bec5e19bdba8f
Teddy Otieno <teddy.asola@andela.com>
2017-11-03 15:04:22 +0300

    deep copy observation to editedObservation object without reference

--- app/js/controllers/showObservation.js
+++ app/js/controllers/showObservation.js
@@ -5,6 +5,15 @@ const ShowObservationCtrl = ($scope, $rootScope, $state, $stateParams, TestData,
 
     $scope.editObservation = () => {
         $scope.toggleEditObservation = true;
+        $scope.editedObservation = angular.copy($scope.observation);
+    };
+
+    $scope.cancelEditingObservation = () => {
+        $scope.toggleEditObservation = false;
+    };
+
+    $scope.submitEditedObservation = (obj) => {
+        console.log(obj);
     };
 };
 
