94f30171a59dc3e29f94fb468290a464fa86b12b
Teddy Otieno <teddy.asola@andela.com>
2017-11-03 03:43:35 +0300

    add editing observation form controls

--- app/js/controllers/showObservation.js
+++ app/js/controllers/showObservation.js
@@ -1,6 +1,10 @@
 const ShowObservationCtrl = ($scope, $rootScope, $state, $stateParams, TestData, DataService) => {
     let observations = DataService;
     $scope.observation = observations.$getRecord($stateParams.id);
+
+    $scope.editObservation = () => {
+        console.log('yay!');
+    };
 };
 
 export default ShowObservationCtrl;
