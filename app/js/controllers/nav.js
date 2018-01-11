123329f2603438d3a9c28f368216d7cbc0d9581d
Teddy Otieno <teddy.asola@andela.com>
2018-01-11 15:12:14 +0300

    toggle menu and settings icons

--- app/js/controllers/nav.js
+++ app/js/controllers/nav.js
@@ -1,9 +1,24 @@
 const NavCtrl = ($scope) => {
     let isMenuDropdownVisible = false;
+    let isSettingsDropdownVisible = false;
+
     $scope.toggleMenuDropdown = () => {
         if (!isMenuDropdownVisible) {
-            // angular.element(document.getElementsByClassName('.c-menu-dropdown')).css('display', 'flex');
+            angular.element(document.getElementsByClassName('c-dropdown--menu')[0]).css('display', 'flex');
             isMenuDropdownVisible = true;
+        } else {
+            isMenuDropdownVisible = false;
+            angular.element(document.getElementsByClassName('c-dropdown--menu')[0]).css('display', 'none');
+        }
+    };
+
+    $scope.toggleSettingsDropdown = () => {
+        if (!isSettingsDropdownVisible) {
+            angular.element(document.getElementsByClassName('c-dropdown--settings')[0]).css('display', 'flex');
+            isSettingsDropdownVisible = true;
+        } else {
+            isSettingsDropdownVisible = false;
+            angular.element(document.getElementsByClassName('c-dropdown--settings')[0]).css('display', 'none');
         }
     };
 };
