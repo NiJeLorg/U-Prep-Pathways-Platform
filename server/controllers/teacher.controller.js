8eef7371323ed7cd06bb9ab91b6889dea4ae176b
Joseph Bosire <kashboss@gmail.com>
2018-02-01 18:59:37 +0300

    update teachers endpoints to use async/await pattern
    Signed-off-by: Joseph Bosire <kashboss@gmail.com>

--- server/controllers/teacher.controller.js
+++ server/controllers/teacher.controller.js
@@ -1,59 +1,46 @@
 import {subject, teacher, school, grade} from './../models';
 
-function get(req, res) {
-    let schoolId = req.params.schoolId || req.query.schoolId;
-    let gradeId = req.params.gradeId || req.query.gradeId;
-    let includes = {};
-    if (schoolId && gradeId){
-        includes = {
-            attributes: ['id', 'name'],
-            include: [
-                {
-                    required: true,
-                    attributes: [],
-                    model: school,
-                    where: {id: schoolId}
-                }, {
-                    attributes: [],
-                    model: grade,
-                    as: 'grades',
-                    where: {id: gradeId},
-                    required: true,
-                }
-
-            ]
-        };
+const get = async (req, res) => {
+    res.sendData(req.teacher);
+};
+const list = async (req, res) => {
+    const teachers =  await teacher.all(getIncludes(req));
+    res.sendData(teachers)
+};
+const load = async (req, res, next, id) => {
+    const teacherObj = await teacher.findById(id, getIncludes(req));
+    if (!teacherObj) {
+        return res.sendNotFound();
     }
-    return teacher
-        .findAll(includes)
-        .then(teachers => res.sendData(teachers))
-        .catch(error => res.sendBadRequest());
+    req.teacher = teacherObj;
+    return next();
+};
 
-}
-
-function load(req, res, next, id) {
-    let schoolId = req.params.schoolId || req.query.schoolId;
-    let gradeId = req.params.gradeId || req.query.gradeId;
-    let includes = {};
-    if (schoolId && gradeId){
-        includes = {
-            attributes: ['id', 'name'],
-            include: [
-                {model: subject,as: 'subjects', attributes: ['id', 'name']},
-                { attributes: [], required: true, model: school, where: {id: schoolId}},
-                { attributes: [], required: true, model: grade, as: 'grades', where: {id: gradeId}}
-            ]
-        }
+const getIncludes = (req) => {
+    const schoolId = req.params.schoolId || req.query.schoolId;
+    const gradeId = req.params.gradeId || req.query.gradeId;
+    let includes = {attributes: ['id', 'name'], include:[
+        {model: subject, as: 'subjects', attributes: ['id', 'name']}
+    ]};
+    if (schoolId){
+        includes.include.push({
+            required: true,
+            attributes: [],
+            model: school,
+            where: {id: schoolId}
+        });
     }
-    return teacher
-        .findById(req.params.teacherId, includes)
-        .then((teacher) => {
-            if (!teacher) {
-                return res.sendNotFound();
-            }
-            return res.sendData(teacher);
-        })
-        .catch((error) => res.sendBadRequest());
-}
+    if (gradeId) {
+        includes.include.push({
+            attributes: [],
+            model: grade,
+            as: 'grades',
+            where: {id: gradeId},
+            required: true,
+        });
+    }
+    return includes;
 
-export default {get, load};
+};
+
+export default {get, load, list};
--- server/routes/teacher.route.js
+++ server/routes/teacher.route.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import teacherCtrl from './../controllers/teacher.controller';
-
+import asyncHandler from 'express-async-handler';
 const router = express.Router({mergeParams: true});
 
 router.route('/')
 /** Get /api/teachers - Get list of teachers*/
-    .get(teacherCtrl.get);
+    .get(asyncHandler(teacherCtrl.list));
 
 router.route('/:teacherId')
 /** GET /api/teachers/:teacherId - Get teacher */
-    .get(teacherCtrl.load);
+    .get(asyncHandler(teacherCtrl.get));
 /** Load teacher when API with teacherId route parameter is hit */
-router.param('teacherId', teacherCtrl.load);
+router.param('teacherId', asyncHandler(teacherCtrl.load));
 export default router;
