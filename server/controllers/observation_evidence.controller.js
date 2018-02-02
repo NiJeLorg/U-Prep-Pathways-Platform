27b535597ce27e12edaad54c0cbb2c50e87e2c61
Joseph Bosire <kashboss@gmail.com>
2018-02-02 14:35:16 +0300

    Refactor observation evidence endpoint to use async/await
    Signed-off-by: Joseph Bosire <kashboss@gmail.com>

--- server/controllers/observation_evidence.controller.js
+++ server/controllers/observation_evidence.controller.js
@@ -1,31 +1,18 @@
 import {observation_evidence} from './../models';
 
-
-function load(req, res, next, id) {
-    return observation_evidence
-        .findById(req.params.observationEvidenceId)
-        .then((observationEvidence) => {
-            if (!observationEvidence) {
-                return res.sendNotFound();
-            }
-            req.observationEvidence = observationEvidence;
-            return next();
-        })
-        .catch((error) => res.sendBadRequest());
-
-}
-
-
-/**
- * Delete observationEvidence.
- * @returns {observationEvidence}
- */
-function remove(req, res, next) {
+const load = async(req, res, next, id) => {
+    const observationEvidence = await observation_evidence.findById(id);
+    if (!observationEvidence) {
+        return res.sendNotFound();
+    }
+    req.observationEvidence = observationEvidence;
+    return next();
+};
+const remove = async(req, res) => {
     const observationEvidence = req.observationEvidence;
-    observationEvidence.destroy()
-        .then(deletedObservationEvidence => res.sendData(deletedObservationEvidence))
-        .catch(e => next(e));
-}
+    const deletedObservationEvidence = await observationEvidence.destroy();
+    res.sendData(deletedObservationEvidence);
+};
 
 
 export default {load,remove};
--- server/routes/observation_evidence.route.js
+++ server/routes/observation_evidence.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import observationEvidenceCtrl from './../controllers/observation_evidence.controller';
+import asyncHandler from 'express-async-handler';
 const router = express.Router();
 
 router.route('/:observationEvidenceId')
 /** DELETE /api/observation_evidences/:observationEvidenceId - Delete observationEvidence */
-    .delete(observationEvidenceCtrl.remove);
+    .delete(asyncHandler(observationEvidenceCtrl.remove));
 /** Load observationEvidence when API with observationEvidenceId route parameter is hit */
-router.param('observationEvidenceId', observationEvidenceCtrl.load);
+router.param('observationEvidenceId', asyncHandler(observationEvidenceCtrl.load));
 export default router;
