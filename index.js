6b0d7fe46e4d409a82f7c5cd7b8c057f4ba0306a
Teddy Otieno <asolatotieno@gmail.com>
2018-11-04 18:57:07 +0300

    Add cors and methodoverride, set path for uploads

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const httpStatus = require("http-status");
-const uprepResponse = require("./server/middleware/response");
 const helmet = require("helmet");
 const pino = require("express-pino-logger")();
+const cors = require("cors");
+const uprepResponse = require("./server/middleware/response");
 const routes = require("./server/routes/index.route");
 
 // const APIError = require('./../server/helpers/APIError')
@@ -11,16 +13,15 @@ const path = require("path");
 
 const app = express();
 
-// parse body params and attache them to req.body
+app.use(cors());
+app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(uprepResponse());
 app.use(pino);
-// secure apps by setting various HTTP headers
 app.use(helmet());
-// serve static files
-app.use(express.static(path.resolve("./public")));
-app.use("/uploads", express.static(path.resolve("./uploads")));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
 // mount all routes on /api path
 app.use("/api", routes);
@@ -30,13 +31,13 @@ app.get("*", (req, res) => {
 });
 
 // if error is not an instanceOf APIError, convert it.
-// app.use((err, req, res, next) => {
-//     if (!(err instanceof APIError)) {
-//         const apiError = new APIError(err.message, err.status, err.isPublic);
-//         return next(apiError);
-//     }
-//     return next(err);
-// });
+app.use((err, req, res, next) => {
+    if (!(err instanceof APIError)) {
+        const apiError = new APIError(err.message, err.status, err.isPublic);
+        return next(apiError);
+    }
+    return next(err);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
