ad310ce3e0c4fa52c52854fb92d002e61dc5ab51
Teddy Otieno <asolatotieno@gmail.com>
2018-11-05 15:48:33 +0300

    Formatting

--- server/routes/seeder.route.js
+++ server/routes/seeder.route.js
@@ -1,19 +1,13 @@
-const express = require('express');
-const seederCtrl = require('./../controllers/seeder.controller');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const seederCtrl = require("./../controllers/seeder.controller");
+const asyncHandler = require("express-async-handler");
 
 const router = express.Router();
 
-router.route('/teachers')
-/** Get /api/schools - Get list of schools*/
-    .post( asyncHandler(seederCtrl.seedTeacher));
+router.route("/teachers").post(asyncHandler(seederCtrl.seedTeacher));
 
-router.route('/grades')
-/** Get /api/schools - Get list of schools*/
-    .post( asyncHandler(seederCtrl.seedGrades));
+router.route("/grades").post(asyncHandler(seederCtrl.seedGrades));
 
-router.route('/subjects')
-/** Get /api/schools - Get list of schools*/
-    .post( asyncHandler(seederCtrl.seedSubjects));
+router.route("/subjects").post(asyncHandler(seederCtrl.seedSubjects));
 
-module.exports =  router;
+module.exports = router;
