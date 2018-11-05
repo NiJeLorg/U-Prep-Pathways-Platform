2e93199de7d1f3b26aae5ff2f37a872e7e38c131
Teddy Otieno <asolatotieno@gmail.com>
2018-11-05 15:45:46 +0300

    Route back to teacher's view when editing a score or observation

--- app/js/controllers/teacher.js
+++ app/js/controllers/teacher.js
@@ -21,31 +21,36 @@ export default [
         let observationToBeDeleted;
         $scope.subview = "observations";
 
-        ObservationTypeService.fetchObservationTypes((err, res) => {
-            if (!err) {
+        ObservationTypeService.fetchObservationTypes().then(
+            res => {
                 $scope.observationTypes = res.data.data;
-            } else {
+            },
+            err => {
                 console.error(err, "ERROR");
             }
-        });
+        );
 
         $scope.newTeacherObservation = teacher => {
             ObservationFactory.teacher = teacher;
             ObservationFactory["observationType"] = $scope.observationTypes[1];
             $state.go("observationInputs", { workflow: "observations" });
+            localStorage.setItem("observationParentRoute", "teacher");
         };
 
         $scope.newTeacherScore = teacher => {
             ScoreFactory.teacher = teacher;
             $state.go("scoreDetails", { workflow: "scores" });
+            localStorage.setItem("scoreParentRoute", "teacher");
         };
 
         $scope.editObservation = observation => {
             $state.go("observationForm", { observationId: observation.id });
+            localStorage.setItem("observationParentRoute", "teacher");
         };
 
         $scope.editScore = score => {
             $state.go("scoreForm", { scoreId: score.id });
+            localStorage.setItem("scoreParentRoute", "teacher");
         };
 
         $scope.openModal = observation => {
