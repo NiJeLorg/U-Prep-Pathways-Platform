0d9112b2e33104834e0e1cc24fc5555f0e3efe9e
Teddy Otieno <asolatotieno@gmail.com>
2018-11-05 15:48:21 +0300

    Make name of school to be unique

--- server/models/school.js
+++ server/models/school.js
@@ -1,38 +1,45 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-    let School = sequelize.define('school', {
-        name: DataTypes.STRING,
-        address: DataTypes.STRING
-    },{
-        underscored: true
-    });
-    School.associate = (models) => {
+    let School = sequelize.define(
+        "school",
+        {
+            name: {
+                type: DataTypes.STRING,
+                unique: true
+            },
+            address: DataTypes.STRING
+        },
+        {
+            underscored: true
+        }
+    );
+    School.associate = models => {
         School.hasMany(models.teacher, {
-            foreignKey: 'school_id',
-            as: 'teachers',
+            foreignKey: "school_id",
+            as: "teachers"
         });
         School.hasMany(models.observation, {
-            foreignKey: 'school_id',
-            as: 'observations',
+            foreignKey: "school_id",
+            as: "observations"
         });
         School.hasMany(models.score, {
-            foreignKey: 'school_id',
-            as: 'scores',
+            foreignKey: "school_id",
+            as: "scores"
         });
         School.belongsToMany(models.grade, {
-            foreignKey: 'school_id',
+            foreignKey: "school_id",
             through: models.grade_school,
-            onDelete: 'CASCADE',
-            as: 'grades'
+            onDelete: "CASCADE",
+            as: "grades"
         });
         School.belongsToMany(models.subject, {
-            foreignKey: 'school_id',
+            foreignKey: "school_id",
             through: models.school_subject,
-            onDelete: 'CASCADE',
-            as: 'subjects'
+            onDelete: "CASCADE",
+            as: "subjects"
         });
         School.belongsTo(models.district, {
-            foreignKey: 'district_id',
+            foreignKey: "district_id"
         });
     };
     return School;
