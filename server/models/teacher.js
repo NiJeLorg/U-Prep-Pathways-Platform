3f8749b5498fff140ce08f9d86408acced8c13ce
Teddy Otieno <asolatotieno@gmail.com>
2018-11-12 18:58:27 +0300

    Formatting changes

--- server/models/teacher.js
+++ server/models/teacher.js
@@ -1,29 +1,33 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-    let Teacher = sequelize.define('teacher', {
-        name: DataTypes.STRING
-    }, {
-        underscored: true
-    });
-    Teacher.associate = (models) => {
+    let Teacher = sequelize.define(
+        "teacher",
+        {
+            name: DataTypes.STRING
+        },
+        {
+            underscored: true
+        }
+    );
+    Teacher.associate = models => {
         Teacher.belongsTo(models.school, {
-            foreignKey: 'school_id',
+            foreignKey: "school_id"
         });
         Teacher.belongsToMany(models.grade, {
-            foreignKey: 'teacher_id',
+            foreignKey: "teacher_id",
             through: models.grade_teacher,
-            as: 'grades'
+            as: "grades"
         });
         Teacher.belongsToMany(models.subject, {
-            foreignKey: 'teacher_id',
+            foreignKey: "teacher_id",
             through: models.subject_teacher,
-            as: 'subjects'
+            as: "subjects"
         });
         Teacher.hasMany(models.observation, {
-            foreignKey: 'teacher_id',
+            foreignKey: "teacher_id"
         });
         Teacher.hasMany(models.score, {
-            foreignKey: 'teacher_id',
+            foreignKey: "teacher_id"
         });
     };
     return Teacher;
