2e710175336a91bd7abe1bc33c0f5e2de1dad5b0
Teddy Otieno <asolatotieno@gmail.com>
2018-11-12 18:58:06 +0300

    Update Teacher

--- server/routes/teacher.route.js
+++ server/routes/teacher.route.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const teacherCtrl = require('./../controllers/teacher.controller');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const teacherCtrl = require("./../controllers/teacher.controller");
+const asyncHandler = require("express-async-handler");
 
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
-router.route('/')
-/** Get /api/teachers - Get list of teachers*/
+router
+    .route("/")
+    /** Get /api/teachers - Get list of teachers*/
     .get(asyncHandler(teacherCtrl.list))
     .post(asyncHandler(teacherCtrl.create));
 
-router.route('/:teacherId')
-/** GET /api/teachers/:teacherId - Get teacher */
+router
+    .route("/:teacherId")
     .get(asyncHandler(teacherCtrl.get))
-    .delete(asyncHandler(teacherCtrl.destroy));
+    .delete(asyncHandler(teacherCtrl.destroy))
+    .put(asyncHandler(teacherCtrl.update));
 
 /** Load teacher when API with teacherId route parameter is hit */
-router.param('teacherId', asyncHandler(teacherCtrl.load));
+router.param("teacherId", asyncHandler(teacherCtrl.load));
 
-module.exports =  router;
+module.exports = router;
