7699a974c1461946edcda02f32365dbccf0667d0
Teddy Otieno <asolatotieno@gmail.com>
2018-11-16 17:31:11 +0300

    Remove commented out code

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,13 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-      username: DataTypes.STRING,
-      first_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      bio: DataTypes.STRING,
-      profile_pic: DataTypes.STRING
-  });
-  // User.associate = (models) => {
-  //   User.belongsTo(models.group, {
-  //     foreignKey: 'groupId',
-  //     onDelete: 'CASCADE',
-  //   });
-  // };
-  return User;
+    var User = sequelize.define("User", {
+        username: DataTypes.STRING,
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        bio: DataTypes.STRING,
+        profile_pic: DataTypes.STRING
+    });
+    return User;
 };
