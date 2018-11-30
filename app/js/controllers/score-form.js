e2ff8f19cf9f0fcb2d2bfada2b7ef629ec289cf9
Teddy Otieno <asolatotieno@gmail.com>
2018-11-30 18:39:49 +0300

    Attach Evidence Feature, Fix Issue with toggling value for scores

--- app/js/controllers/score-form.js
+++ app/js/controllers/score-form.js
@@ -1,12 +1,32 @@
 export default [
     "$scope",
+    "$controller",
     "$mdDialog",
     "score",
     "ElementService",
     "IndicatorScoreService",
-    function($scope, $mdDialog, score, ElementService, IndicatorScoreService) {
+    "TeacherService",
+    "ObservationService",
+    "UtilService",
+    "ScoreService",
+    "$stateParams",
+    function(
+        $scope,
+        $controller,
+        $mdDialog,
+        score,
+        ElementService,
+        IndicatorScoreService,
+        TeacherService,
+        ObservationService,
+        UtilService,
+        ScoreService,
+        $stateParams
+    ) {
         $scope.score = score.data;
         $scope.indicatorValue = [null, 1, 2, 3, 4];
+        let teacher = {};
+        let observations;
 
         // fetch data
         ElementService.fetchElements().then(
@@ -14,7 +34,38 @@ export default [
                 $scope.elements = res.data.data;
                 checkComponentsToBeExpanded($scope.elements);
             },
-            err => console.error(err, "ERROR")
+            err => console.error(err)
+        );
+
+        const fetchScore = () => {
+            ScoreService.query({
+                id: $stateParams.scoreId
+            }).$promise.then(res => {
+                $scope.score = res.data;
+            });
+        };
+
+        TeacherService.get(
+            {
+                id: $scope.score.teacher_id
+            },
+            res => {
+                teacher = res.data;
+            },
+            err => {
+                console.error(err);
+            }
+        );
+
+        ObservationService.fetchTeacherObservations(
+            $scope.score.teacher_id
+        ).then(
+            res => {
+                observations = res.data.data;
+            },
+            err => {
+                console.error(err);
+            }
         );
 
         const checkComponentsToBeExpanded = elements => {
@@ -48,6 +99,42 @@ export default [
             }
         };
 
+        $scope.getAddSelectedIndicators = indicators => {
+            let indicators_mapped = indicators.map(indicator => {
+                let matched_indicator = $scope.score.indicator_scores.filter(
+                    indicator_score =>
+                        indicator_score.indicator_id === indicator.id
+                );
+                if (matched_indicator.length > 0) {
+                    indicator.value = "" + matched_indicator[0].value;
+                }
+                return indicator;
+            });
+            // console.log(indicators_mapped);
+            return indicators_mapped;
+        };
+        $scope.checkIndicatorScore = (indicator, value) => {
+            let matched_indicator_score = $scope.score.indicator_scores.filter(
+                indicator_score => indicator_score.indicator_id === indicator.id
+            );
+            return false;
+        };
+
+        $scope.storeIndicatorScore = (value, indicator) => {
+            IndicatorScoreService.createIndicatorScore({
+                value: value,
+                score_id: $scope.score.id,
+                indicator_id: indicator.id
+            }).then(
+                res => {
+                    fetchScore();
+                },
+                err => {
+                    console.error(err);
+                }
+            );
+        };
+
         // show modals
         $scope.showIndicatorLevelsModal = ev => {
             $mdDialog.show({
@@ -74,6 +161,22 @@ export default [
                 locals: { score: $scope.score },
                 controller: ModalController,
                 templateUrl: "views/modal-submitScore.html",
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        };
+
+        $scope.showAttachEvidenceModal = (ev, indicator) => {
+            $mdDialog.show({
+                locals: {
+                    score: $scope.score,
+                    indicator,
+                    teacher,
+                    observations
+                },
+                controller: AttachEvidenceController,
+                templateUrl: "views/modal-attachEvidence.html",
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose: true
@@ -128,35 +231,48 @@ export default [
             };
         }
 
-        $scope.getAddSelectedIndicators = indicators => {
-            let indicators_mapped = indicators.map(indicator => {
-                let matched_indicator = $scope.score.indicator_scores.filter(
-                    indicator_score =>
-                        indicator_score.indicator_id === indicator.id
-                );
-                if (matched_indicator.length > 0) {
-                    indicator.value = "" + matched_indicator[0].value;
-                }
-                return indicator;
-            });
-            return indicators_mapped;
-        };
-        $scope.checkIndicatorScore = (indicator, value) => {
-            let matched_indicator_score = $scope.score.indicator_scores.filter(
-                indicator_score => indicator_score.indicator_id === indicator.id
-            );
-            return false;
-        };
+        function AttachEvidenceController(
+            $scope,
+            $mdDialog,
+            indicator,
+            teacher,
+            observations,
+            IndicatorScoreService
+        ) {
+            $scope.indicator = indicator;
+            $scope.teacher = teacher;
+            $scope.observations = observations;
 
-        $scope.storeIndicatorScore = (value, indicator) => {
-            IndicatorScoreService.createIndicatorScore(
-                {
-                    value: value,
-                    score_id: $scope.score.id,
-                    indicator_id: indicator.id
-                },
-                (err, res) => {}
-            );
-        };
+            $scope.selectAttachment = (e, attachment) => {
+                attachment.toggle
+                    ? angular.element(e.target).css("opacity", "0")
+                    : angular.element(e.target).css("opacity", "1");
+                attachment.toggle = attachment.toggle ? false : true;
+            };
+
+            $scope.closeModal = () => {
+                $mdDialog.cancel();
+            };
+
+            $scope.attachEvidence = () => {
+                IndicatorScoreService.update(
+                    {
+                        
+                    },
+                    res => {
+                        console.log(res);
+                    },
+                    err => {
+                        console.error(err);
+                    }
+                );
+            };
+
+            $scope.isDocument = fileName => UtilService.isDocument(fileName);
+
+            $scope.isImage = fileName => UtilService.isImage(fileName);
+
+            $scope.isVideo = fileName => UtilService.isVideo(fileName);
+        }
     }
 ];
