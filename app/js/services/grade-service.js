8c70c93e283ea39395e06485e072f83e3feb6f84
Teddy Otieno <asolatotieno@gmail.com>
2019-02-26 23:02:35 +0300

    Refactor grade service

--- app/js/services/grade-service.js
+++ app/js/services/grade-service.js
@@ -2,17 +2,11 @@ export default [
     "$resource",
     "$http",
     function($resource, $http) {
-        let obj = $resource("/api/grades/:id");
+        let obj = $resource("/api/grades/:id", {
+            id: "@id"
+        });
 
         obj.fetchGrades = () => $http.get("/api/grades");
-
-        obj.createGrade = newGrade =>
-            $http.post("/api/grades", { grade: newGrade });
-
-        obj.updateGrade = grade =>
-            $http.put("/api/grades/" + grade.id, {
-                name: grade.name
-            });
 
         return obj;
     }
